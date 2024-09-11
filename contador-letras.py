4920fc3aaa6523079c034bdc809a16b0fae0eef5
Agustín <agustinreiden@gmail.com>
2024-09-11 20:51:34 -0300

    Realizamos un contador de palabras como practica de algoritmos, pseudocodigo y diagrama de flujo

---
+++ contador-letras.py
@@ -0,0 +1,8 @@
+from itertools import count
+
+
+palabra_ingresada = input("Por favor ingrese una palabra: ")
+
+cantidad_letras = len(palabra_ingresada)
+
+print("La palabra ingresada tiene:", cantidad_letras, "letras")
